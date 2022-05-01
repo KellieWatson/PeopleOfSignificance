93a7dae4632d14cd6f4e770517be71c18321da78
kelliewatson <kelliewatson@gmail.com>
2022-05-01 09:43:15 -0700

    Initial Project Creation

---
+++ src/main/java/com/watsonxyz/kd/PeopleOfSignificance/PeopleOfSignificanceApplication.java
@@ -0,0 +1,13 @@
+package com.watsonxyz.kd.PeopleOfSignificance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleOfSignificanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleOfSignificanceApplication.class, args);
+	}
+
+}
